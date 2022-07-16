f0df623c77f2a4294ae2cb2c6d5d3bddf5cc8f79
Александр Камликов <kundili093@gmail.com>
2022-07-16 13:33:19 +0300

    fix orders

--- routes/getOrders.js
+++ routes/getOrders.js
@@ -4,8 +4,26 @@ const axios = require('axios');
 
 router.get('/', async (req, res, next) => {
 
+    const { select } = req.query;
+
     const orders = await axios.get('https://elif-tech-default-rtdb.europe-west1.firebasedatabase.app/orders.json');
 
-    res.send(JSON.stringify(orders.data));
+    if (select) {
+        const response = {
+
+        };
+
+        Object.entries(orders.data).forEach(([name, value]) => {
+            if (name === select || value.phone === select || value.address === value) {
+                response[name] = value;
+            }
+        });
+
+        res.send(JSON.stringify(response));
+    } else {
+
+        res.send(JSON.stringify(orders.data));
+    }
+
 });
 module.exports = router;
